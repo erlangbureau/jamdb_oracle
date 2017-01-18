23f6b2462424d0d88bb39f43ed4ec05bc7727a39
vstavskyi <vstavskyi@users.noreply.github.com>
2017-01-18 11:19:48 +0200

    Connection options in hrl file

---
+++ test/jamdb_oracle_test.hrl
@@ -0,0 +1,9 @@
+-define(ConnOpts, [
+	{host, "jamdb-oracle-dev.erlangbureau.dp.ua"},
+	{port, 1521},
+	{user, "jamdbtest"},
+	{password, "jamdbtest"},
+	{sid, "JAMDBTEST"},
+%%	{service_name, "JAMDBTEST"},
+	{app_name, "jamdbtest"}
+]).
