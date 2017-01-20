3793670730e02e1dfaa6dc9b491ebe5b8f5c16e9
vstavskyi <vstavskyi@users.noreply.github.com>
2017-01-20 13:43:25 +0200

    Add tests

--- test/jamdb_oracle_test.hrl
+++
@@ -1,9 +0,0 @@
--define(ConnOpts, [
-	{host, "jamdb-oracle-dev.erlangbureau.dp.ua"},
-	{port, 1521},
-	{user, "jamdbtest"},
-	{password, "jamdbtest"},
-	{sid, "JAMDBTEST"},
-%%	{service_name, "JAMDBTEST"},
-	{app_name, "jamdbtest"}
-]).
