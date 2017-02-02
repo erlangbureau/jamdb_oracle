a5e30663c0e531d1228dc45b0b9862b386a75183
vstavskyi <vstavskyi@users.noreply.github.com>
2017-02-02 17:43:05 +0200

    mix support

---
+++ mix.exs
@@ -0,0 +1,33 @@
+defmodule JamdbOracle.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :jamdb_oracle,
+     version: "0.0.3",
+     elixir: "~> 1.0",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  def application do
+    [applications: [:jose]]
+  end
+
+  defp deps do
+    [{:jose, "~> 1.8.0"}]
+  end
+
+  defp description do
+    "Erlang driver for Oracle"
+  end
+
+  defp package do
+    [files: ["src","include","test","rebar.config","mix.exs"],
+    maintainers: ["Mykhailo Vstavskyi","Sergiy Kostyushkin"],
+    licenses: ["MIT"],
+    links: %{"Github" => "https://github.com/erlangbureau/jamdb_oracle"}]
+  end
+end
