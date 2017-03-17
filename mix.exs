4643d21458ccf1dfbeb37e4f7cb61c5737c4daa3
vstavskyi <vstavskyi@users.noreply.github.com>
2017-03-17 16:27:32 +0200

    ecto adapter first version

--- mix.exs
+++ mix.exs
@@ -1,9 +1,9 @@
-defmodule JamdbOracle.Mixfile do
+defmodule Jamdb.Oracle.Mixfile do
   use Mix.Project
 
   def project do
     [app: :jamdb_oracle,
-     version: "0.0.4",
+     version: "0.0.5",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -13,19 +13,22 @@ defmodule JamdbOracle.Mixfile do
   end
 
   def application do
-    [applications: [:jose]]
+    [applications: [:db_connection]]
   end
 
   defp deps do
-    [{:jose, "~> 1.8.0"}]
+    [{:jose, "~> 1.8.0"},
+     {:ecto, "~> 2.1.0"},
+     {:db_connection, "~> 1.1.0"},
+     {:ex_doc, "~> 0.14.0", only: :docs}]
   end
 
   defp description do
-    "Erlang driver for Oracle"
+    "Erlang driver and Ecto adapter for Oracle"
   end
 
   defp package do
-    [files: ["src","include","test","rebar.config","mix.exs"],
+    [files: ["src","include","lib","test","rebar.config","mix.exs"],
     maintainers: ["Mykhailo Vstavskyi","Sergiy Kostyushkin"],
     licenses: ["MIT"],
     links: %{"Github" => "https://github.com/erlangbureau/jamdb_oracle"}]
