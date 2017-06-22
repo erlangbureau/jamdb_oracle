977a145021cec1d954b7a6c7eeb866ddb15c114f
vstavskyi <vstavskyi@users.noreply.github.com>
2017-06-22 15:56:41 +0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Jamdb.Oracle.Mixfile do
     [{:jose, "~> 1.8.0"},
      {:ecto, "~> 2.1.0"},
      {:db_connection, "~> 1.1.0"},
-     {:ex_doc, "~> 0.14.0", only: :docs}]
+     {:ex_doc, "~> 0.16.0", only: :docs}]
   end
 
   defp description do
