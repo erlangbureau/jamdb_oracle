fc56051bca99b9f778e21af490a22e56f1b166db
vstavskyi <vstavskyi@users.noreply.github.com>
2017-10-01 18:29:25 +0300

    Update test_helper.exs

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,54 +1,30 @@
-Logger.configure(level: :debug)
-ExUnit.start
+ExUnit.start()
 
-# Configure Ecto for support and tests
-Application.put_env(:ecto, :primary_key_type, :id)
-
-# Configure Oracle connection
 Code.require_file "jamdb_oracle_test.exs", __DIR__
 
-# Load support files
-Code.require_file "../deps/ecto/integration_test/support/repo.exs", __DIR__
+alias Jamdb.Oracle.TestRepo
 
-# Pool repo for async, safe tests
-alias Ecto.Integration.TestRepo
-
-Application.put_env(:ecto, TestRepo,
+Application.put_env(:jamdb, TestRepo,
   adapter: Ecto.Adapters.Jamdb.Oracle,
-  url: Application.get_env(:ecto, :jamdb_oracle_test_url),
-  pool_size: 1,
-  pool: Ecto.Adapters.SQL.Sandbox,
-  ownership_pool: DBConnection.Poolboy)
-
-defmodule Ecto.Integration.TestRepo do
-  use Ecto.Integration.Repo, otp_app: :ecto
-end
-
-# Pool repo for non-async tests
-alias Ecto.Integration.PoolRepo
-
-Application.put_env(:ecto, PoolRepo,
-  adapter: Ecto.Adapters.Jamdb.Oracle,
-  pool: DBConnection.Poolboy,
   url: Application.get_env(:ecto, :jamdb_oracle_test_url),
   timeout: 15000,
-  pool_size: 10,
-  max_restarts: 20,
-  max_seconds: 10)
+  pool_timeout: 5000,
+  pool_size: 1,
+  pool: DBConnection.Ownership,
+  ownership_pool: DBConnection.Poolboy)
 
-defmodule Ecto.Integration.PoolRepo do
-  use Ecto.Integration.Repo, otp_app: :ecto
+defmodule Jamdb.Oracle.TestRepo do
+  use Ecto.Repo, otp_app: :jamdb
 end
 
-defmodule Ecto.Integration.Case do
-  use ExUnit.CaseTemplate
+{:ok, _pid} = TestRepo.start_link()
 
-  setup do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(TestRepo)
+defmodule Jamdb.Oracle.TestCase do
+  use ExUnit.Case
+  import Ecto.Query
+
+  test "query" do
+    assert [] = TestRepo.all(from dual in "dual", select: :nil, where: is_nil(^1)) |> IO.inspect
+    assert {:ok, _ret } = TestRepo.query("select 1+:1, sysdate, rowid from dual where 1=:1 ", [1]) |> IO.inspect
   end
 end
-
-{:ok, _pid} = TestRepo.start_link
-{:ok, _pid} = PoolRepo.start_link
-
-TestRepo.query("select 1+:1, sysdate, rowid from dual where 1=:1 ", [1]) |> IO.inspect
