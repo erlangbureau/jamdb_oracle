8e82f945be243df4b38d78db921ad8b049789bdc
vstavskyi <vstavskyi@users.noreply.github.com>
2017-10-25 00:21:28 +0300

    prepared statement functionality fix

--- src/jamdb_oracle_tns_decoder.erl
+++ src/jamdb_oracle_tns_decoder.erl
@@ -12,6 +12,8 @@
 decode_packet(<<PacketSize:16, 0:16, ?TNS_DATA, _Flags:8, 0:16, _DataFlags:16, Rest/bits>>) ->
     BodySize = PacketSize-10,
     case Rest of
+        <<PacketBody:BodySize/binary, Rest2/bits>> when PacketSize =:= 8155 ->	%SDU TDU 65535
+            {more, ?TNS_DATA, PacketBody, Rest2};
         <<PacketBody:BodySize/binary, Rest2/bits>> ->
             {ok, ?TNS_DATA, PacketBody, Rest2};
         _ ->
