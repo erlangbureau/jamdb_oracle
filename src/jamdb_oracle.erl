b61e027ba82987d04a83957860c213f248b5e6d9
vstavskyi <vstavskyi@users.noreply.github.com>
2018-01-30 14:47:14 +0200

    Update jamdb_oracle.erl

--- src/jamdb_oracle.erl
+++ src/jamdb_oracle.erl
@@ -11,8 +11,6 @@
 -export([handle_call/3, handle_cast/2, handle_info/2]).
 -export([code_change/3]).
 
--include("jamdb_oracle_defaults.hrl").
-
 %% API
 -spec start_link(jamdb_oracle_conn:options()) -> {ok, pid()} | {error, term()}.
 start_link(Opts) when is_list(Opts) ->
