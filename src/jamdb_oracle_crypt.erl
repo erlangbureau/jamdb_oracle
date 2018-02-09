671cae2f9b30218527f600c7261d5754fe3cab30
vstavskyi <vstavskyi@users.noreply.github.com>
2018-02-09 15:25:32 +0200

    12c password version implementation complete

--- src/jamdb_oracle_crypt.erl
+++ src/jamdb_oracle_crypt.erl
@@ -1,7 +1,9 @@
 -module(jamdb_oracle_crypt).
 
--export([o5logon/5]).
+-export([o5logon/2]).
 -export([validate/2]).
+
+-include("jamdb_oracle.hrl").
 
 %%====================================================================
 %% callbacks
@@ -15,22 +17,24 @@
 %    AuthPass = crypto:block_encrypt(des_cbc, binary:part(SrvSess,0,8), IVec, CliPass),
 %    {bin2hexstr(AuthPass)++[N], [], []}.
 
-o5logon(Sess, User, User, Pass, Bits) when is_list(Sess), Bits =:= 128 ->
+o5logon(#logon{auth=Sess, user=User, password=Pass}, Bits) when Bits =:= 128 ->
     IVec = <<0:64>>,
     CliPass = norm(User++Pass),
     Rest1 = crypto:block_encrypt(des_cbc, hexstr2bin("0123456789ABCDEF"), IVec, CliPass),
     Rest2 = crypto:block_encrypt(des_cbc, binary:part(Rest1,byte_size(Rest1),-8), IVec, CliPass),
     KeySess = <<(binary:part(Rest2,byte_size(Rest2),-8))/binary,0:64>>,
-    o5logon(hexstr2bin(Sess), KeySess, [], Pass, Bits);
-o5logon(Sess, Salt, Salt, Pass, Bits) when is_list(Sess), Bits =:= 192 ->
+    o5logon(#logon{auth=hexstr2bin(Sess), key=KeySess, password=Pass, bits=Bits});
+o5logon(#logon{auth=Sess, salt=Salt, password=Pass}, Bits) when Bits =:= 192 ->
     Data = crypto:hash(sha,<<(list_to_binary(Pass))/binary,(hexstr2bin(Salt))/binary>>),
     KeySess = <<Data/binary,0:32>>,
-    o5logon(hexstr2bin(Sess), KeySess, [], Pass, Bits);
-o5logon(Sess, Salt, DerivedSalt, Pass, Bits) when is_list(Sess), Bits =:= 256 ->
+    o5logon(#logon{auth=hexstr2bin(Sess), key=KeySess, password=Pass, bits=Bits});
+o5logon(#logon{auth=Sess, salt=Salt, der_salt=DerivedSalt, password=Pass}, Bits) when Bits =:= 256 ->
     Data = pbkdf2(sha512, 4096, 64, Pass, <<(hexstr2bin(Salt))/binary,"AUTH_PBKDF2_SPEEDY_KEY">>),
     KeySess = binary:part(crypto:hash(sha512, <<Data/binary, (hexstr2bin(Salt))/binary>>),0,32),
-    o5logon(hexstr2bin(Sess), KeySess, DerivedSalt, Pass, Bits);
-o5logon(Sess, KeySess, DerivedSalt, Pass, Bits) when is_binary(Sess) ->
+    o5logon(#logon{auth=hexstr2bin(Sess), key=KeySess, password=Pass, bits=Bits,
+    der_salt=DerivedSalt, der_key = <<(pad(16,<<>>))/binary, Data/binary>>}).
+
+o5logon(#logon{auth=Sess, key=KeySess, der_salt=DerivedSalt, der_key=DerivedKey, password=Pass, bits=Bits}) ->
     IVec = <<0:128>>,
     SrvSess = jose_jwa_aes:block_decrypt({aes_cbc, Bits}, KeySess, IVec, Sess),
     CliSess = crypto:strong_rand_bytes(byte_size(SrvSess)),
@@ -38,14 +42,17 @@ o5logon(Sess, KeySess, DerivedSalt, Pass, Bits) when is_binary(Sess) ->
     CatKey = cat_key(SrvSess, CliSess, Bits),
     KeyConn = conn_key(CatKey, DerivedSalt, Bits),
     AuthPass = jose_jwa_aes:block_encrypt({aes_cbc, Bits}, KeyConn, IVec, pad(Pass)),
-    {bin2hexstr(AuthPass), bin2hexstr(AuthSess), bin2hexstr(KeyConn)}.
-
-%    SpeedyKey = jose_jwa_aes:block_encrypt({aes_cbc, Bits}, KeyConn, IVec, binary:part(Data,16,48)),
+    SpeedyKey =
+    case DerivedKey of
+        undefined -> <<>>;
+        _ -> jose_jwa_aes:block_encrypt({aes_cbc, Bits}, KeyConn, IVec, DerivedKey)
+    end,
+    {bin2hexstr(AuthPass), bin2hex(AuthSess), bin2hexstr(SpeedyKey), KeyConn}.
 
 validate(Resp, KeyConn) ->
     IVec = <<0:128>>,
-    Bits = length(KeyConn) * 4,
-    Data = jose_jwa_aes:block_decrypt({aes_cbc, Bits}, hexstr2bin(KeyConn), IVec, hexstr2bin(Resp)),    
+    Bits = byte_size(KeyConn) * 8,
+    Data = jose_jwa_aes:block_decrypt({aes_cbc, Bits}, KeyConn, IVec, hexstr2bin(Resp)),
     case binary:match(Data,<<"SERVER_TO_CLIENT">>) of
 	nomatch -> error;
 	_ -> ok
@@ -55,9 +62,9 @@ validate(Resp, KeyConn) ->
 %% Internal misc
 %%====================================================================
 
-conn_key(Data, [], Bits) when Bits =:= 128 ->
+conn_key(Data, undefined, Bits) when Bits =:= 128 ->
     <<(erlang:md5(Data))/binary>>;
-conn_key(Data, [], Bits) when Bits =:= 192 ->
+conn_key(Data, undefined, Bits) when Bits =:= 192 ->
     <<(erlang:md5(binary:part(Data,0,16)))/binary,
       (binary:part(erlang:md5(binary:part(Data,16,8)),0,8))/binary>>;
 conn_key(Data, DerivedSalt, Bits) when Bits =:= 256 ->
