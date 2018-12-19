4a6ee0147537636f11efefc3ee88b741c0bd83a4
vstavskyi <vstavskyi@users.noreply.github.com>
2018-12-19 11:13:37 +0200

    Update test_helper.exs

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,7 @@
 defmodule Jamdb.Oracle.TestRepo do
-  use Ecto.Repo, otp_app: :jamdb_oracle
+  use Ecto.Repo,
+  otp_app: :jamdb_oracle,
+  adapter: Ecto.Adapters.Jamdb.Oracle
 end
-{:ok, _pid} = Jamdb.Oracle.TestRepo.start_link()
+Supervisor.start_link([Jamdb.Oracle.TestRepo], strategy: :one_for_one)
 ExUnit.start()
