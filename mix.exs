bfe43868d246eb833980f058b5ed467e40c8d197
vstavskyi <vstavskyi@users.noreply.github.com>
2019-01-22 16:09:55 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Jamdb.Oracle.Mixfile do
 
   def project do
     [app: :jamdb_oracle,
-     version: "0.3.1",
+     version: "0.3.2",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
