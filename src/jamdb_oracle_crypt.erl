448d29235135115735c409d6df98449332873246
Peter Lemenkov <lemenkov@gmail.com>
2019-07-06 14:37:55 +0200

    Switch to erlang's crypto and drop jose
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/jamdb_oracle_crypt.erl
+++ src/jamdb_oracle_crypt.erl
@@ -36,27 +36,26 @@ o5logon(#logon{auth=Sess, salt=Salt, der_salt=DerivedSalt, password=Pass}, Bits)
 
 o5logon(#logon{auth=Sess, key=KeySess, der_salt=DerivedSalt, der_key=DerivedKey, password=Pass, bits=Bits}) ->
     IVec = <<0:128>>,
-    SrvSess = jose_jwa_aes:block_decrypt({aes_cbc, Bits}, KeySess, IVec, Sess),
+    SrvSess = crypto:block_decrypt(aes_cbc, KeySess, IVec, Sess),
     CliSess =
     case binary:match(SrvSess,pad(8, <<>>)) of
         {40,8} -> pad(8, crypto:strong_rand_bytes(40));
         _ -> crypto:strong_rand_bytes(byte_size(SrvSess))
     end,
-    AuthSess = jose_jwa_aes:block_encrypt({aes_cbc, Bits}, KeySess, IVec, CliSess),
+    AuthSess = crypto:block_encrypt(aes_cbc, KeySess, IVec, CliSess),
     CatKey = cat_key(SrvSess, CliSess, DerivedSalt, Bits),
     KeyConn = conn_key(CatKey, DerivedSalt, Bits),
-    AuthPass = jose_jwa_aes:block_encrypt({aes_cbc, Bits}, KeyConn, IVec, pad(Pass)),
+    AuthPass = crypto:block_encrypt(aes_cbc, KeyConn, IVec, pad(Pass)),
     SpeedyKey =
     case DerivedKey of
         undefined -> <<>>;
-        _ -> jose_jwa_aes:block_encrypt({aes_cbc, Bits}, KeyConn, IVec, DerivedKey)
+        _ -> crypto:block_encrypt(aes_cbc, KeyConn, IVec, DerivedKey)
     end,
     {bin2hexstr(AuthPass), bin2hex(AuthSess), bin2hexstr(SpeedyKey), KeyConn}.
 
 validate(Resp, KeyConn) ->
     IVec = <<0:128>>,
-    Bits = byte_size(KeyConn) * 8,
-    Data = jose_jwa_aes:block_decrypt({aes_cbc, Bits}, KeyConn, IVec, hexstr2bin(Resp)),
+    Data = crypto:block_decrypt(aes_cbc, KeyConn, IVec, hexstr2bin(Resp)),
     case binary:match(Data,<<"SERVER_TO_CLIENT">>) of
 	nomatch -> error;
 	_ -> ok
