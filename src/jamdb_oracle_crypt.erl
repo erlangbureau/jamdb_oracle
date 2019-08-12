b2d6ff04739eaab6a0dd42fdc25fbfbaa4952bbd
vstavskyi <vstavskyi@users.noreply.github.com>
2019-08-12 09:03:33 +0300

    fix 18c logon

--- src/jamdb_oracle_crypt.erl
+++ src/jamdb_oracle_crypt.erl
@@ -24,10 +24,10 @@ o5logon(#logon{auth=Sess, der_salt=DerivedSalt, user=User, password=Pass}, Bits)
     Rest2 = crypto:block_encrypt(des_cbc, binary:part(Rest1,byte_size(Rest1),-8), IVec, CliPass),
     KeySess = <<(binary:part(Rest2,byte_size(Rest2),-8))/binary,0:64>>,
     o5logon(#logon{auth=hexstr2bin(Sess), key=KeySess, password=Pass, bits=Bits, der_salt=DerivedSalt});
-o5logon(#logon{auth=Sess, salt=Salt, password=Pass}, Bits) when Bits =:= 192 ->
+o5logon(#logon{auth=Sess, salt=Salt, der_salt=DerivedSalt, password=Pass}, Bits) when Bits =:= 192 ->
     Data = crypto:hash(sha,<<(list_to_binary(Pass))/binary,(hexstr2bin(Salt))/binary>>),
     KeySess = <<Data/binary,0:32>>,
-    o5logon(#logon{auth=hexstr2bin(Sess), key=KeySess, password=Pass, bits=Bits});
+    o5logon(#logon{auth=hexstr2bin(Sess), key=KeySess, password=Pass, bits=Bits, der_salt=DerivedSalt});
 o5logon(#logon{auth=Sess, salt=Salt, der_salt=DerivedSalt, password=Pass}, Bits) when Bits =:= 256 ->
     Data = pbkdf2(sha512, 4096, 64, Pass, <<(hexstr2bin(Salt))/binary,"AUTH_PBKDF2_SPEEDY_KEY">>),
     KeySess = binary:part(crypto:hash(sha512, <<Data/binary, (hexstr2bin(Salt))/binary>>),0,32),
@@ -70,17 +70,13 @@ conn_key(Data, undefined, Bits) when Bits =:= 128 ->
 conn_key(Data, undefined, Bits) when Bits =:= 192 ->
     <<(erlang:md5(binary:part(Data,0,16)))/binary,
       (binary:part(erlang:md5(binary:part(Data,16,8)),0,8))/binary>>;
-conn_key(Data, DerivedSalt, Bits) when Bits =:= 256; Bits =:= 128 ->
+conn_key(Data, DerivedSalt, Bits) ->
     pbkdf2(sha512, 3, Bits div 8, bin2hexstr(Data), hexstr2bin(DerivedSalt)).
 
-cat_key(X,Y,undefined,Bits) when Bits =:= 128 ->
-    cat_key(binary:part(X,16,16),binary:part(Y,16,16),[]);
-cat_key(X,Y,_DerivedSalt,Bits) when Bits =:= 128 ->
-    <<(binary:part(Y,0,16))/binary,(binary:part(X,0,16))/binary>>;
-cat_key(X,Y,undefined,Bits) when Bits =:= 192 ->
-    cat_key(binary:part(X,16,24),binary:part(Y,16,24),[]);
-cat_key(X,Y,_DerivedSalt,Bits) when Bits =:= 256 ->
-    <<Y/binary,X/binary>>.
+cat_key(X,Y,undefined, Bits) ->
+    cat_key(binary:part(X, 16, Bits div 8),binary:part(Y, 16, Bits div 8),[]);
+cat_key(X,Y,_DerivedSalt, Bits) ->
+    <<(binary:part(Y, 0, Bits div 8))/binary,(binary:part(X, 0, Bits div 8))/binary>>.
 
 cat_key(<<>>,<<>>,S) ->
     list_to_binary(S);
