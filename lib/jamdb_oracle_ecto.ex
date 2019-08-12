7e7c4f51231afd0565a02a5c6c647f819d4f0bea
vstavskyi <vstavskyi@users.noreply.github.com>
2019-08-12 10:52:17 +0300

    Update jamdb_oracle_ecto.ex

--- lib/jamdb_oracle_ecto.ex
+++ lib/jamdb_oracle_ecto.ex
@@ -181,6 +181,7 @@ defmodule Ecto.Adapters.Jamdb.Oracle.Connection do
   defdelegate insert(prefix, table, header, rows, on_conflict, returning), to: Jamdb.Oracle.Query
   defdelegate update(prefix, table, fields, filters, returning), to: Jamdb.Oracle.Query
   defdelegate delete(prefix, table, filters, returning), to: Jamdb.Oracle.Query
+  defdelegate table_exists_query(table), to: Jamdb.Oracle.Query
 
   @impl true
   def to_constraints(_err), do: []
