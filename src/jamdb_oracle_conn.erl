aadc5c78ebe1a54c5a647d8ce4c8dbf3d3d29b04
vstavskyi <vstavskyi@users.noreply.github.com>
2019-08-19 11:10:09 +0300

    clean up timeouts, add parameter read_timeout

--- src/jamdb_oracle_conn.erl
+++ src/jamdb_oracle_conn.erl
@@ -42,29 +42,30 @@ connect(Opts, Tout) ->
     Auto        = proplists:get_value(autocommit, Opts, ?DEF_AUTOCOMMIT),
     Fetch       = proplists:get_value(fetch, Opts, ?DEF_FETCH),
     Sdu         = proplists:get_value(sdu, Opts, ?DEF_SDU),
+    ReadTout    = proplists:get_value(read_timeout, Opts, ?DEF_READ_TIMEOUT),
     Charset     = proplists:get_value(charset, Opts, utf8),
     SockOpts = [binary, {active, false}, {packet, raw}, %{recbuf, 65535},
             {nodelay, true}, {keepalive, true}]++SocketOpts,
     case gen_tcp:connect(Host, Port, SockOpts, Tout) of
         {ok, Socket} ->
             {ok, Socket2} = sock_connect(Socket, SslOpts, Tout),
-            State = #oraclient{socket=Socket2, env=Opts, auto=Auto, fetch=Fetch, sdu=Sdu, req=Charset, timeout=Tout},
+            State = #oraclient{socket=Socket2, env=Opts, auto=Auto, fetch=Fetch, sdu=Sdu, req=Charset, timeouts={Tout, ReadTout}},
             {ok, State2} = send_req(login, State),
-            handle_login(State2#oraclient{conn_state=auth_negotiate}, Tout);
+            handle_login(State2#oraclient{conn_state=auth_negotiate});
         {error, Reason} ->
             {error, socket, Reason}
     end.
 
 -spec disconnect(state()) -> {ok, [env()]}.
-disconnect(#oraclient{timeout=Tout} = State) ->
-    disconnect(State, Tout).
+disconnect(State) ->
+    disconnect(State, 1).
 
 -spec disconnect(state(), timeout()) -> {ok, [env()]}.
 disconnect(#oraclient{socket=Socket, env=Env}, 0) ->
     sock_close(Socket),
     {ok, Env};
-disconnect(#oraclient{conn_state=connected, socket=Socket, env=Env} = State, Tout) ->
-    _ = send_req(close, State, Tout),
+disconnect(#oraclient{conn_state=connected, socket=Socket, env=Env} = State, _Tout) ->
+    _ = send_req(close, State),
     sock_close(Socket),
     {ok, Env};
 disconnect(#oraclient{env=Env}, _Tout) ->
@@ -75,39 +76,39 @@ reconnect(State) ->
     {ok, InitOpts} = disconnect(State, 0),
     connect(InitOpts).
 
+-spec sql_query(state(), string() | tuple(), timeout()) -> query_result().
+sql_query(State, Query, _Tout) ->
+    sql_query(State, Query).
+
 -spec sql_query(state(), string() | tuple()) -> query_result().
 sql_query(State, Query) when is_list(Query) ->
     sql_query(State, {Query, []});
-sql_query(#oraclient{timeout=Tout} = State, Query) ->
-    sql_query(State, Query, Tout).
-
--spec sql_query(state(), string() | tuple(), timeout()) -> query_result().
-sql_query(State, {Query, Bind}, Tout) when length(Query) > 10 ->
-    sql_query(State, {Query, Bind, [], []}, Tout);
-sql_query(State, {batch, Query, [Bind|Batch]}, Tout) ->
-    sql_query(State, {Query, Bind, Batch, []}, Tout);
-sql_query(State, {fetch, Query, Bind}, Tout) ->
-    sql_query(State, {Query, Bind, [], fetch}, Tout);
-sql_query(#oraclient{conn_state=connected} = State, {fetch, Cursor, RowFormat, LastRow}, Tout) ->
+sql_query(State, {Query, Bind}) when length(Query) > 10 ->
+    sql_query(State, {Query, Bind, [], []});
+sql_query(State, {batch, Query, [Bind|Batch]}) ->
+    sql_query(State, {Query, Bind, Batch, []});
+sql_query(State, {fetch, Query, Bind}) ->
+    sql_query(State, {Query, Bind, [], fetch});
+sql_query(#oraclient{conn_state=connected} = State, {fetch, Cursor, RowFormat, LastRow}) ->
     {ok, State2} = send_req(fetch, State#oraclient{type=fetch}, Cursor),
-    handle_resp({Cursor, RowFormat, [LastRow]}, State2, Tout);
-sql_query(#oraclient{conn_state=connected} = State, {Query, Bind, Batch, Fetch}, Tout) ->
+    handle_resp({Cursor, RowFormat, [LastRow]}, State2);
+sql_query(#oraclient{conn_state=connected} = State, {Query, Bind, Batch, Fetch}) ->
     {ok, State2} = send_req(exec, State, {Query, Bind, Batch}),
     #oraclient{server=Ver, defcols=DefCol, params=RowFormat, type=Type} = State2,
     handle_resp(get_param(defcols, {DefCol, Ver, RowFormat, Type}),
-    State2#oraclient{type=get_param(type, {Type, Fetch})}, Tout);
-sql_query(#oraclient{conn_state=connected} = State, {Query, Bind}, Tout) ->
+    State2#oraclient{type=get_param(type, {Type, Fetch})});
+sql_query(#oraclient{conn_state=connected, timeouts={_Tout, ReadTout}} = State, {Query, Bind}) ->
     case lists:nth(1, string:tokens(string:to_upper(Query)," \t;")) of
-        "SESSION" -> sql_query(State, {?ENCODER:encode_helper(sess, []), [], [], []}, Tout);
-        "COMMIT" -> handle_req(tran, State, ?TTI_COMMIT, Tout);
-        "ROLLBACK" -> handle_req(tran, State, ?TTI_ROLLBACK, Tout);
-        "COMON" -> handle_req(tran, State#oraclient{auto=1}, ?TTI_COMON, Tout);
-        "COMOFF" -> handle_req(tran, State#oraclient{auto=0}, ?TTI_COMOFF, Tout);
-        "PING" -> handle_req(tran, State, ?TTI_PING, Tout);
-        "STOP" -> handle_req(stop, State, hd(Bind), Tout);
-        "START" -> handle_req(spfp, State, [], Tout), handle_req(start, State, hd(Bind), Tout);
-        "CLOSE" -> disconnect(State, Tout), {ok, [], State#oraclient{conn_state=disconnected}};
-        "TIMEOUT" -> {ok, [], State#oraclient{timeout=hd(Bind)}};
+        "SESSION" -> sql_query(State, {?ENCODER:encode_helper(sess, []), [], [], []});
+        "COMMIT" -> handle_req(tran, State, ?TTI_COMMIT);
+        "ROLLBACK" -> handle_req(tran, State, ?TTI_ROLLBACK);
+        "COMON" -> handle_req(tran, State#oraclient{auto=1}, ?TTI_COMON);
+        "COMOFF" -> handle_req(tran, State#oraclient{auto=0}, ?TTI_COMOFF);
+        "PING" -> handle_req(tran, State, ?TTI_PING);
+        "STOP" -> handle_req(stop, State, hd(Bind));
+        "START" -> handle_req(spfp, State, []), handle_req(start, State, hd(Bind));
+        "CLOSE" -> disconnect(State, 1), {ok, [], State#oraclient{conn_state=disconnected}};
+        "TIMEOUT" -> {ok, [], State#oraclient{timeouts={hd(Bind), ReadTout}}};
         "FETCH" -> {ok, [], State#oraclient{fetch=hd(Bind)}};
         _ -> {ok, undefined, State}
     end.
@@ -116,26 +117,26 @@ loop(Values) ->
     receive {get, From} -> From ! Values, loop(Values); {set, Values2} -> loop(Values2) end.
 
 %% internal
-handle_login(#oraclient{socket=Socket, env=Env, sdu=Length} = State, Tout) ->
-    case recv(Socket, Length, Tout) of
+handle_login(#oraclient{socket=Socket, env=Env, sdu=Length, timeouts=Touts} = State) ->
+    case recv(Socket, Length, Touts) of
         {ok, ?TNS_DATA, BinaryData} ->
             case handle_token(BinaryData, State) of
-                {ok, State2} -> handle_login(State2, Tout);
+                {ok, State2} -> handle_login(State2);
                 State2 -> {ok, State2}                  %connected
             end;
         {ok, ?TNS_REDIRECT, BinaryData} ->
             {ok, Opts} = ?DECODER:decode_token(net, {BinaryData, Env}),
             reconnect(State#oraclient{env=Opts});
         {ok, ?TNS_RESEND, _BinaryData} ->
-            {ok, Socket2} = sock_renegotiate(Socket, Env, Tout),
+            {ok, Socket2} = sock_renegotiate(Socket, Env, Touts),
             {ok, State2} = send_req(login, State#oraclient{socket=Socket2}),
-            handle_login(State2, Tout);
+            handle_login(State2);
         {ok, ?TNS_ACCEPT, <<_Ver:16,_Opts:16,Sdu:16,_Rest/bits>> = _BinaryData} ->
             Task = spawn(fun() -> loop(0) end),
             {ok, State2} = send_req(pro, State#oraclient{seq=Task,sdu=Sdu}),
-            handle_login(State2, Tout);
+            handle_login(State2);
         {ok, ?TNS_MARKER, _BinaryData} ->
-            handle_req(marker, State, [], Tout);
+            handle_req(marker, State, []);
         {ok, ?TNS_REFUSE, BinaryData} ->
             _ = handle_error(remote, BinaryData, State),
             disconnect(State, 0);
@@ -150,7 +151,7 @@ handle_token(<<Token, Data/binary>>, State) ->
 	?TTI_RPA ->
             case ?DECODER:decode_token(rpa, Data) of
                 {?TTI_SESS, Request} ->
-		    send_req(auth, State#oraclient{auth=Request});
+		    send_req(auth, State#oraclient{req=Request});
                 {?TTI_AUTH, Resp, Ver, SessId} ->
                     #oraclient{auth = KeyConn} = State,
                     Cursors = spawn(fun() -> loop([]) end),
@@ -171,53 +172,53 @@ handle_error(Type, Reason, State) ->
 %    io:format("~s~n", [Reason]),
     {error, Type, Reason, State}.
 
-handle_req(pig, #oraclient{cursors=Cursors,seq=Task} = State, {Type, Request}, Tout) ->
+handle_req(pig, #oraclient{cursors=Cursors,seq=Task} = State, {Type, Request}) ->
     {LPig, LPig2} = unzip([get_param(defcols, DefCol) || DefCol <- get_result(Cursors)]),
     Pig = if length(LPig) > 0 -> get_record(pig, [], {?TTI_CANA, LPig}, Task); true -> <<>> end,
     Pig2 = if length(LPig2) > 0 -> get_record(pig, [], {?TTI_OCCA, LPig2}, Task); true -> <<>> end,
     Data = get_record(Type, [], Request, Task),
     {ok, State2} = send(State, ?TNS_DATA, <<Pig/binary, Pig2/binary, Data/binary>>),
-    handle_resp([], State2, Tout);
-handle_req(marker, State, Acc, Tout) ->
+    handle_resp([], State2);
+handle_req(marker, State, Acc) ->
     {ok, State2} = send(State, ?TNS_MARKER, <<1,0,2>>),
-    handle_resp(Acc, State2, Tout);
-handle_req(fob, State, Acc, Tout) ->
+    handle_resp(Acc, State2);
+handle_req(fob, State, Acc) ->
     {ok, State2} = send(State, ?TNS_DATA, <<?TTI_FOB>>),
-    handle_resp(Acc, State2, Tout);
-handle_req(Type, #oraclient{seq=Task} = State, Request, Tout) ->
+    handle_resp(Acc, State2);
+handle_req(Type, #oraclient{seq=Task} = State, Request) ->
     Data = get_record(Type, [], Request, Task),
     {ok, State2} = send(State, ?TNS_DATA, Data),
-    handle_resp([], State2, Tout).
-    
+    handle_resp([], State2).
+
 unzip(Ts) -> unzip(Ts, [], []).
-	
+
 unzip([{X, 0} | Ts], Xs, Ys) -> unzip(Ts, [X | Xs], Ys);
 unzip([{X, Y} | Ts], Xs, Ys) -> unzip(Ts, [X | Xs], [Y | Ys]);
 unzip([], Xs, Ys) -> {Ys, Ys ++ Xs}.
-	
-send_req(login, #oraclient{env=Env,sdu=Length} = State) ->
-    Data = ?ENCODER:encode_record(login, #oraclient{env=Env,sdu=Length}),
-    send(State, ?TNS_CONNECT, Data);
-send_req(auth, #oraclient{env=Env,auth=Request,seq=Task} = State) ->
-    {Data, KeyConn} = get_record(auth, Env, Request, Task),
-    send(State#oraclient{auth=KeyConn}, ?TNS_DATA, Data);
-send_req(Type, #oraclient{env=Env,req=Request,seq=Task} = State) ->
-    Data = get_record(Type, Env, Request, Task),
-    send(State, ?TNS_DATA, Data).
 
-send_req(close, #oraclient{server=0,seq=Task} = State, _Tout) ->
+send_req(login, State) ->
+    Data = get_record(login, State, [], 0),
+    send(State, ?TNS_CONNECT, Data);
+send_req(auth, #oraclient{req=Request,seq=Task} = State) ->
+    {Data, KeyConn} = get_record(auth, State, Request, Task),
+    send(State#oraclient{auth=KeyConn,req=[]}, ?TNS_DATA, Data);
+send_req(close, #oraclient{server=0,seq=Task} = State) ->
     exit(Task, ok),
     send(State, ?TNS_DATA, <<64>>);
-send_req(close, #oraclient{auto=0} = State, Tout) ->
-    _ = handle_req(tran, State, ?TTI_ROLLBACK, Tout),
-    send_req(close, State#oraclient{auto=1}, Tout);
-send_req(close, #oraclient{cursors=Cursors} = State, Tout) ->
-    _ = handle_req(pig, State, {close, 0}, Tout),
+send_req(close, #oraclient{auto=0} = State) ->
+    _ = handle_req(tran, State, ?TTI_ROLLBACK),
+    send_req(close, State#oraclient{auto=1});
+send_req(close, #oraclient{cursors=Cursors} = State) ->
+    _ = handle_req(pig, State, {close, 0}),
     exit(Cursors, ok),
-    send_req(close, State#oraclient{server=0}, Tout);
-send_req(reset, #oraclient{cursors=Cursors} = State, Tout) ->
-    handle_req(pig, State, {tran, ?TTI_PING}, Tout),
+    send_req(close, State#oraclient{server=0});
+send_req(reset, #oraclient{cursors=Cursors} = State) ->
+    handle_req(pig, State, {tran, ?TTI_PING}),
     Cursors ! {set, []};
+send_req(Type, #oraclient{req=Request,seq=Task} = State) ->
+    Data = get_record(Type, State, Request, Task),
+    send(State, ?TNS_DATA, Data).
+
 send_req(fetch, #oraclient{seq=Task} = State, {Cursor, RowFormat}) ->
     Data = get_record(exec, State#oraclient{type=fetch}, {Cursor, [], [], [], RowFormat}, Task),
     send(State, ?TNS_DATA, Data);
@@ -238,18 +239,18 @@ send_req(exec, #oraclient{fetch=Fetch,cursors=Cursors,seq=Task} = State, {Query,
         [get_param(data, B) || B <- Bind], Batch, []}, Task),
     send(State#oraclient{type=Type,defcols=DefCol,params=[get_param(format, B) || B <- Bind]},
         ?TNS_DATA, <<Pig/binary, Pig2/binary, Data/binary>>).
-	
-handle_resp(Acc, #oraclient{socket=Socket, sdu=Length} = State, Tout) ->
-    case recv(Socket, Length, Tout) of
+
+handle_resp(Acc, #oraclient{socket=Socket, sdu=Length, timeouts=Touts} = State) ->
+    case recv(Socket, Length, Touts) of
         {ok, ?TNS_DATA, Data} ->
-            handle_resp(Data, Acc, State, Tout);
+            handle_resp(Data, Acc, State);
         {ok, ?TNS_MARKER, _Data} ->
-            handle_req(marker, State, Acc, Tout);
+            handle_req(marker, State, Acc);
         {error, Type, Reason} ->
             handle_error(Type, Reason, State)
     end.
 
-handle_resp(Data, Acc, #oraclient{type=Type, cursors=Cursors} = State, Tout) ->
+handle_resp(Data, Acc, #oraclient{type=Type, cursors=Cursors} = State) ->
     case ?DECODER:decode_token(Data, Acc) of
 	{0, RowNumber, Cursor, {LCursor, RowFormat}, []} when Type =/= change, RowFormat =/= [] ->
 	    {Type2, Request} =
@@ -260,32 +261,32 @@ handle_resp(Data, Acc, #oraclient{type=Type, cursors=Cursors} = State, Tout) ->
 	    {ok, State2} = send_req(fetch, State, Request),
 	    #oraclient{auto=Auto, defcols=DefCol} = State2,
 	    {_, Result} = get_result(Auto, DefCol, {LCursor, Cursor, RowFormat}, Cursors),
-            handle_resp({Cursor, RowFormat, []}, State2#oraclient{defcols=Result, type=Type2}, Tout);
+            handle_resp({Cursor, RowFormat, []}, State2#oraclient{defcols=Result, type=Type2});
 	{RetCode, RowNumber, Cursor, {LCursor, RowFormat}, Rows} ->
 	    case get_result(Type, RetCode, RowNumber, RowFormat, Rows) of
 		more when Type =:= fetch ->
 		    {ok, [{fetched_rows, Cursor, RowFormat, Rows}], State};
 		more ->
 		    {ok, State2} = send_req(fetch, State, Cursor),
-		    handle_resp({Cursor, RowFormat, Rows}, State2, Tout);
+		    handle_resp({Cursor, RowFormat, Rows}, State2);
 		{ok, _} = Result ->
 		    #oraclient{auto=Auto, defcols=DefCol} = State,
 		    case get_result(Auto, DefCol, {LCursor, Cursor, RowFormat}, Cursors) of
-			{reset, _} -> send_req(reset, State, Tout);
+			{reset, _} -> send_req(reset, State);
 			_ -> more
 		    end,
 		    erlang:append_element(Result, State);
 		{error, Result} ->
 		    case get_result(Cursors) of
 			[] -> more;
-			_ -> send_req(reset, State, Tout)
+			_ -> send_req(reset, State)
 		    end,
 		    {ok, Result, State}
 	    end;
 	{ok, Result} -> %tran
 	    {ok, Result, State};
 	{error, fob} -> %return
-	    handle_req(fob, State, Acc, Tout);
+	    handle_req(fob, State, Acc);
 	{error, Reason} ->
 	    handle_error(remote, Reason, State)
     end.
@@ -326,7 +327,8 @@ get_result(_Auto, DefCol, _Result, _Cursors) -> {more, DefCol}.
 get_param(Task) when is_pid(Task) ->
     Task ! {get, self()},
     Tseq = receive 127 -> 0; Reply -> Reply end,
-    Task ! {set, Tseq + 1}, Tseq + 1.
+    Task ! {set, Tseq + 1}, Tseq + 1;
+get_param(Tseq) -> Tseq.
 
 get_param([], _M, Acc) -> Acc;
 get_param([H|L], M, Acc) -> get_param(L, M, Acc++[maps:get(list_to_atom(H), M)]).
@@ -360,13 +362,13 @@ get_param(Type, Data) ->
     {<<>>, DataType, Length, Scale, Charset} = ?DECODER:decode_helper(param, Data),
     #format{param=Type,data_type=DataType,data_length=Length,data_scale=Scale,charset=Charset}.
 
-get_record(Type, State, Request, Task) when is_record(State, oraclient) ->
-    ?ENCODER:encode_record(Type, State#oraclient{req=Request, seq=get_param(Task)});
-get_record(Type, Env, Request, Task) ->
-    ?ENCODER:encode_record(Type, #oraclient{env=Env, req=Request, seq=get_param(Task)}).
+get_record(Type, [], Request, Task) ->
+    ?ENCODER:encode_record(Type, #oraclient{req=Request, seq=get_param(Task)});
+get_record(Type, State, Request, Task) ->
+    ?ENCODER:encode_record(Type, State#oraclient{req=Request, seq=get_param(Task)}).
 
-sock_renegotiate(Socket, _Opts, _Tout) when is_port(Socket) -> {ok, Socket};
-sock_renegotiate(Socket, Opts, Tout) ->
+sock_renegotiate(Socket, _Opts, _Touts) when is_port(Socket) -> {ok, Socket};
+sock_renegotiate(Socket, Opts, {Tout, _ReadTout}) ->
     SslOpts = proplists:get_value(ssl, Opts, []),
     {ok, Socket2} = ssl:close(Socket, {self(), Tout}),
     ssl:connect(Socket2, SslOpts, Tout).
@@ -394,38 +396,37 @@ send(#oraclient{socket=Socket,sdu=Length} = State, PacketType, Data) ->
             handle_error(socket, Reason, State)
     end.
 
-recv(Socket, Length, Tout) ->
-    recv(Socket, Length, Tout, <<>>, <<>>).
+recv(Socket, Length, Touts) ->
+    recv(Socket, Length, Touts, <<>>, <<>>).
 
-recv(Socket, Length, Acc, Data) ->
-    Tout = 500,
-    case sock_recv(Socket, 0, Tout) of
+recv(read_timeout, Socket, Length, {_Tout, ReadTout} = Touts, Acc, Data) ->
+    case sock_recv(Socket, 0, ReadTout) of
         {ok, NetworkData} ->
-            recv(Socket, Length, Tout, <<Acc/bits, NetworkData/bits>>, Data);
+            recv(Socket, Length, Touts, <<Acc/bits, NetworkData/bits>>, Data);
         {error, timeout} ->
             {ok, ?TNS_DATA, Data};
         {error, Reason} ->
             {error, socket, Reason}
     end.
 
-recv(Socket, Length, Tout, Acc, Data) ->
+recv(Socket, Length, {Tout, _ReadTout} = Touts, Acc, Data) ->
     case ?DECODER:decode_packet(Acc, Length) of
         {ok, ?TNS_MARKER, _PacketBody, _Rest} ->
             {ok, ?TNS_MARKER, <<>>};
         {ok, Type, PacketBody, <<>>} ->
             {ok, Type, <<Data/bits, PacketBody/bits>>};
         {ok, _Type, PacketBody, Rest} ->
-            recv(Socket, Length, Tout, Rest, <<Data/bits, PacketBody/bits>>);
+            recv(Socket, Length, Touts, Rest, <<Data/bits, PacketBody/bits>>);
         {error, more, PacketBody, <<>>} ->
-            recv(Socket, Length, <<>>, <<Data/bits, PacketBody/bits>>);
+            recv(read_timeout, Socket, Length, Touts, <<>>, <<Data/bits, PacketBody/bits>>);
         {error, more, PacketBody, Rest} ->
-            recv(Socket, Length, Tout, Rest, <<Data/bits, PacketBody/bits>>);
+            recv(Socket, Length, Touts, Rest, <<Data/bits, PacketBody/bits>>);
         {error, more} ->
-            recv(Socket, Length, Acc, Data);
+            recv(read_timeout, Socket, Length, Touts, Acc, Data);
         {error, socket} ->
             case sock_recv(Socket, 0, Tout) of
                 {ok, NetworkData} ->
-                    recv(Socket, Length, Tout, <<Acc/bits, NetworkData/bits>>, Data);
+                    recv(Socket, Length, Touts, <<Acc/bits, NetworkData/bits>>, Data);
                 {error, Reason} ->
                     {error, socket, Reason}
             end
