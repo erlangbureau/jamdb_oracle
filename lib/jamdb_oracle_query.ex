49a87a4c773432dbf036888d623b3896effd1fd9
vstavskyi <vstavskyi@users.noreply.github.com>
2019-11-20 14:07:31 +0200

    init support for ecto.migrate

--- lib/jamdb_oracle_query.ex
+++ lib/jamdb_oracle_query.ex
@@ -179,7 +179,7 @@ defmodule Jamdb.Oracle.Query do
   end
 
   defp update_op(command, _key, _value, _sources, query) do
-    error!(query, "Unknown update operation #{inspect command}")
+    error!(query, "unknown update operation #{inspect command}")
   end
 
   defp join(%{joins: []}, _sources), do: []
@@ -335,8 +335,8 @@ defmodule Jamdb.Oracle.Query do
   defp expr({:in, _, [left, right]}, sources, query) when is_list(right) do
     args =
       intersperse_map(right, ?,, fn
-        el when is_list(el) -> [?(, intersperse_map(el, ?,, &expr(&1, sources, query)), ?)]
-        el -> expr(el, sources, query)
+        elem when is_list(elem) -> [?(, intersperse_map(elem, ?,, &expr(&1, sources, query)), ?)]
+        elem -> expr(elem, sources, query)
       end)
     [expr(left, sources, query), " IN (", args, ?)]
   end
@@ -496,6 +496,298 @@ defmodule Jamdb.Oracle.Query do
   end
   defp create_alias(_) do
     "t"
+  end
+
+  # DDL
+
+  alias Ecto.Migration.{Table, Index, Reference, Constraint}
+
+  def execute_ddl({command, %Table{} = table, columns}) when command in [:create, :create_if_not_exists] do
+    table_name = quote_table(table.prefix, table.name)
+    query = [if_do(command == :create_if_not_exists, :begin),
+             "CREATE TABLE ",
+             table_name, ?\s, ?(,
+             column_definitions(table, columns), pk_definition(columns, ", "), ?),
+             options_expr(table.options),
+             if_do(command == :create_if_not_exists, :end)]
+    
+    [query] ++
+      comments_on("TABLE", table_name, table.comment) ++
+      comments_for_columns(table_name, columns)
+  end
+
+  def execute_ddl({command, %Table{} = table}) when command in [:drop, :drop_if_exists] do
+    [[if_do(command == :drop_if_exists, :begin),
+      "DROP TABLE ", quote_table(table.prefix, table.name),
+      if_do(command == :drop_if_exists, :end)]]
+  end
+
+  def execute_ddl({:alter, %Table{} = table, changes}) do
+    table_name = quote_table(table.prefix, table.name)
+    query = ["ALTER TABLE ", table_name, ?\s,
+             column_changes(table, changes), pk_definition(changes, ", ADD ")]
+
+    [query] ++
+      comments_on("TABLE", table_name, table.comment) ++
+      comments_for_columns(table_name, changes)
+  end
+
+  def execute_ddl({command, %Index{} = index}) when command in [:create, :create_if_not_exists] do
+    [[if_do(command == :create_if_not_exists, :begin),
+      "CREATE", if_do(index.unique, " UNIQUE"), " INDEX ",
+      quote_name(index.name),
+      " ON ",
+      quote_table(index.prefix, index.table), ?\s,
+      ?(, intersperse_map(index.columns, ", ", &index_expr/1), ?),
+      if_do(command == :create_if_not_exists, :end)]]
+  end
+
+  def execute_ddl({command, %Index{} = index}) when command in [:drop, :drop_if_exists] do
+    [[if_do(command == :drop_if_exists, :begin),
+      "DROP INDEX ", quote_table(index.prefix, index.name),
+      if_do(command == :drop_if_exists, :end)]]
+  end
+
+  def execute_ddl({:rename, %Table{} = current_table, %Table{} = new_table}) do
+    [["RENAME ", quote_table(current_table.prefix, current_table.name),
+      " TO ", quote_table(nil, new_table.name)]]
+  end
+
+  def execute_ddl({:rename, %Table{} = table, current_column, new_column}) do
+    [["ALTER TABLE ", quote_table(table.prefix, table.name), " RENAME COLUMN ",
+      quote_name(current_column), " TO ", quote_name(new_column)]]
+  end
+
+  def execute_ddl({command, %Constraint{} = constraint}) when command in [:create, :create_if_not_exists] do
+    [[if_do(command == :create_if_not_exists, :begin),
+      "ALTER TABLE ", quote_table(constraint.prefix, constraint.table),
+      " ADD CONSTRAINT ", quote_name(constraint.name), constraint_expr(constraint),
+      if_do(command == :create_if_not_exists, :end)]]
+  end
+
+  def execute_ddl({command, %Constraint{} = constraint}) when command in [:drop, :drop_if_exists] do
+    [[if_do(command == :drop_if_exists, :begin),
+      "ALTER TABLE ", quote_table(constraint.prefix, constraint.table),
+      " DROP CONSTRAINT ", quote_name(constraint.name),
+      if_do(command == :drop_if_exists, :end)]]
+  end
+
+  def execute_ddl(string) when is_binary(string), do: [string]
+
+  def execute_ddl(keyword) when is_list(keyword),
+    do: error!(nil, "keyword lists in execute are not supported")
+
+  defp pk_definition(columns, prefix) do
+    pks =
+      for {_, name, _, opts} <- columns,
+          opts[:primary_key],
+          do: name
+
+    case pks do
+      [] -> []
+      _  -> [prefix, "PRIMARY KEY (", intersperse_map(pks, ", ", &quote_name/1), ")"]
+    end
+  end
+
+  defp comments_on(_object, _name, nil), do: []
+  defp comments_on(object, name, comment) do
+    [["COMMENT ON ", object, ?\s, name, " IS ", single_quote(comment)]]
+  end
+
+  defp comments_for_columns(table_name, columns) do
+    Enum.flat_map(columns, fn
+      {_operation, column_name, _column_type, opts} ->
+        column_name = [table_name, ?. | quote_name(column_name)]
+        comments_on("COLUMN", column_name, opts[:comment])
+      _ -> []
+    end)
+  end
+
+  defp column_definitions(table, columns) do
+    intersperse_map(columns, ", ", &column_definition(table, &1))
+  end
+
+  defp column_definition(table, {:add, name, %Reference{} = ref, opts}) do
+    [quote_name(name), ?\s, column_type(ref.type, opts),
+     column_options(ref.type, opts), reference_expr(ref, table, name)]
+  end
+
+  defp column_definition(_table, {:add, name, type, opts}) do
+    [quote_name(name), ?\s, column_type(type, opts), column_options(type, opts)]
+  end
+
+  defp column_changes(table, columns) do
+    intersperse_map(columns, ", ", &column_change(table, &1))
+  end
+
+  defp column_change(table, {:add, name, %Reference{} = ref, opts}) do
+    ["ADD COLUMN ", quote_name(name), ?\s, column_type(ref.type, opts),
+     column_options(ref.type, opts), reference_expr(ref, table, name)]
+  end
+
+  defp column_change(_table, {:add, name, type, opts}) do
+    ["ADD COLUMN ", quote_name(name), ?\s, column_type(type, opts),
+     column_options(type, opts)]
+  end
+
+  defp column_change(table, {:modify, name, %Reference{} = ref, opts}) do
+    [drop_constraint_expr(opts[:from], table, name), "ALTER COLUMN ", quote_name(name), " TYPE ", column_type(ref.type, opts),
+     constraint_expr(ref, table, name), modify_null(name, opts), modify_default(name, ref.type, opts)]
+  end
+
+  defp column_change(table, {:modify, name, type, opts}) do
+    [drop_constraint_expr(opts[:from], table, name), "ALTER COLUMN ", quote_name(name), " TYPE ",
+     column_type(type, opts), modify_null(name, opts), modify_default(name, type, opts)]
+  end
+
+  defp column_change(_table, {:remove, name}), do: ["DROP COLUMN ", quote_name(name)]
+  defp column_change(table, {:remove, name, %Reference{} = ref, _opts}) do
+    [drop_constraint_expr(ref, table, name), "DROP COLUMN ", quote_name(name)]
+  end
+  defp column_change(_table, {:remove, name, _type, _opts}), do: ["DROP COLUMN ", quote_name(name)]
+
+  defp modify_null(name, opts) do
+    case Keyword.get(opts, :null) do
+      true  -> [", ALTER COLUMN ", quote_name(name), " DROP NOT NULL"]
+      false -> [", ALTER COLUMN ", quote_name(name), " SET NOT NULL"]
+      nil   -> []
+    end
+  end
+
+  defp modify_default(name, _type, opts) do
+    case Keyword.fetch(opts, :default) do
+      {:ok, val} -> [", ALTER COLUMN ", quote_name(name), " SET", default_expr({:ok, val})]
+      :error -> []
+    end
+  end
+
+  defp column_options(_type, opts) do
+    default = Keyword.fetch(opts, :default)
+    null    = Keyword.get(opts, :null)
+    [default_expr(default), null_expr(null)]
+  end
+
+  defp null_expr(false), do: " NOT NULL"
+  defp null_expr(true), do: " NULL"
+  defp null_expr(_), do: []
+
+  defp default_expr({:ok, nil}),
+    do: " DEFAULT NULL"
+  defp default_expr({:ok, literal}) when is_binary(literal),
+    do: [" DEFAULT '", escape_string(literal), ?']
+  defp default_expr({:ok, literal}) when is_number(literal) or is_boolean(literal),
+    do: [" DEFAULT ", to_string(literal)]
+  defp default_expr({:ok, {:fragment, expr}}),
+    do: [" DEFAULT ", expr]
+  defp default_expr({:ok, value}) when is_map(value),
+    do: error!(nil, "json defaults are not supported")
+  defp default_expr(:error),
+    do: []
+
+  defp index_expr(literal) when is_binary(literal),
+    do: literal
+  defp index_expr(literal),
+    do: quote_name(literal)
+
+  defp options_expr(nil),
+    do: []
+  defp options_expr(keyword) when is_list(keyword),
+    do: error!(nil, "keyword lists in :options are not supported")
+  defp options_expr(options),
+    do: [?\s, options]
+
+  defp column_type({:array, type}, opts),
+    do: [column_type(type, opts), "[]"]
+
+  defp column_type(type, _opts) when type in ~w(utc_datetime naive_datetime)a,
+    do: [ecto_to_db(type), "(0)"]
+
+  defp column_type(type, opts) when type in ~w(utc_datetime_usec naive_datetime_usec)a do
+    precision = Keyword.get(opts, :precision)
+    type_name = ecto_to_db(type)
+
+    if precision do
+      [type_name, ?(, to_string(precision), ?)]
+    else
+      type_name
+    end
+  end
+
+  defp column_type(type, opts) do
+    size      = Keyword.get(opts, :size)
+    precision = Keyword.get(opts, :precision)
+    scale     = Keyword.get(opts, :scale)
+    national  = Keyword.get(opts, :national, false)
+    type_name = [if_do(national and type in [:string, :binary], "n"), ecto_to_db(type)]
+
+    cond do
+      size            -> [type_name, ?(, to_string(size), ?)]
+      precision       -> [type_name, ?(, to_string(precision), ?,, to_string(scale || 0), ?)]
+      type == :string -> [type_name, "(255)"]
+      true            -> type_name
+    end
+  end
+
+  defp reference_expr(%Reference{} = ref, table, name),
+    do: [" CONSTRAINT ", reference_name(ref, table, name), " REFERENCES ",
+         quote_table(ref.prefix || table.prefix, ref.table), ?(, quote_name(ref.column), ?),
+         reference_on_delete(ref.on_delete)]
+
+  defp constraint_expr(%Reference{} = ref, table, name),
+    do: [", ADD CONSTRAINT ", reference_name(ref, table, name), ?\s,
+         "FOREIGN KEY (", quote_name(name), ") REFERENCES ",
+         quote_table(ref.prefix || table.prefix, ref.table), ?(, quote_name(ref.column), ?),
+         reference_on_delete(ref.on_delete)]
+
+  defp drop_constraint_expr(%Reference{} = ref, table, name),
+    do: ["DROP CONSTRAINT ", reference_name(ref, table, name), ", "]
+  defp drop_constraint_expr(_, _, _),
+    do: []
+
+  defp reference_name(%Reference{name: nil}, table, column),
+    do: quote_name("#{table.name}_#{column}_fkey")
+  defp reference_name(%Reference{name: name}, _table, _column),
+    do: quote_name(name)
+
+  defp constraint_expr(%Constraint{check: check}) when is_binary(check), 
+    do: [" CHECK ", ?(, check, ?)]
+  defp constraint_expr(_),
+    do: []
+
+  defp reference_on_delete(:nilify_all), do: " ON DELETE SET NULL"
+  defp reference_on_delete(:delete_all), do: " ON DELETE CASCADE"
+  defp reference_on_delete(_), do: []
+
+  defp ecto_to_db(:id),                  do: "integer"
+  defp ecto_to_db(:binary_id),           do: "raw(16)"
+  defp ecto_to_db(:uuid),                do: "raw(16)"
+  defp ecto_to_db(:bigint),              do: "integer"
+  defp ecto_to_db(:bigserial),           do: "integer"
+  defp ecto_to_db(:integer),             do: "integer"
+  defp ecto_to_db(:float),               do: "number"
+  defp ecto_to_db(:boolean),             do: "char(1)"
+  defp ecto_to_db(:string),              do: "varchar2"
+  defp ecto_to_db(:binary),              do: "clob"
+  defp ecto_to_db({:array, _}),          do: "blob"
+  defp ecto_to_db(:map),                 do: "clob"
+  defp ecto_to_db({:map, _}),            do: "clob"
+  defp ecto_to_db(:decimal),             do: "decimal"
+  defp ecto_to_db(:naive_datetime),      do: "timestamp"
+  defp ecto_to_db(:naive_datetime_usec), do: "timestamp"
+  defp ecto_to_db(:utc_datetime),        do: "timestamp with time zone"
+  defp ecto_to_db(:utc_datetime_usec),   do: "timestamp with time zone"
+  defp ecto_to_db(other),                do: Atom.to_string(other)
+
+  defp single_quote(value), do: [?', escape_string(value), ?']
+
+  defp if_do(condition, :begin) do
+    if condition, do: "BEGIN EXECUTE IMMEDIATE '", else: []
+  end
+  defp if_do(condition, :end) do
+    if condition, do: "'; EXCEPTION WHEN OTHERS THEN NULL; END;", else: []
+  end
+  defp if_do(condition, value) do
+    if condition, do: value, else: []
   end
 
   ## Helpers
