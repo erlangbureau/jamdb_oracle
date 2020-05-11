44e12fd7b127c75ab09dc21586694339cee879b0
vstavskyi <vstavskyi@users.noreply.github.com>
2020-05-11 13:39:16 +0300

    return datetime in utc with timezone

--- lib/jamdb_oracle.ex
+++ lib/jamdb_oracle.ex
@@ -1,5 +1,5 @@
 defmodule Jamdb.Oracle do
-  @vsn "0.4.0"
+  @vsn "0.4.1"
   @moduledoc """
   Adapter module for Oracle. `DBConnection` behaviour implementation.
 
@@ -50,6 +50,7 @@ defmodule Jamdb.Oracle do
       {:ok, [{:proc_result, _, msg}]} -> {:error, msg}
       {:ok, [{:affected_rows, num_rows}]} -> {:ok, %{num_rows: num_rows, rows: nil}}
       {:ok, result} -> {:ok, result}
+      {:error, :local, _} -> {:error, "Data is incomplete. Pass :read_timeout as connection parameter."}
       {:error, _, err} -> {:disconnect, err}
     end
   end
@@ -266,6 +267,7 @@ defimpl DBConnection.Query, for: Jamdb.Oracle.Query do
   defp decode(:null), do: nil
   defp decode({elem}) when is_number(elem), do: elem
   defp decode({date, time}) when is_tuple(date), do: to_naive({date, time})
+  defp decode({date, time, tz}) when is_tuple(date) and is_list(tz), do: to_date({date, time, tz})
   defp decode({date, time, _}) when is_tuple(date), do: to_utc({date, time})
   defp decode(elem) when is_list(elem), do: to_binary(elem)
   defp decode(elem), do: elem
@@ -313,6 +315,11 @@ defimpl DBConnection.Query, for: Jamdb.Oracle.Query do
 
   defp to_utc({date, time}),
     do: DateTime.from_naive!(to_naive({date, time}), "Etc/UTC")
+
+  defp to_date({{year, month, day}, {hour, min, sec}, tz}),
+    do: %DateTime{year: year, month: month, day: day, hour: hour, minute: min,
+	second: trunc(sec), microsecond: parse_sec(sec), time_zone: to_binary(tz),
+	zone_abbr: "UTC", utc_offset: 0, std_offset: 0}
 
   defp parse_sec(sec),
     do: {trunc((sec - trunc(sec)) * 1000000) , 6}
