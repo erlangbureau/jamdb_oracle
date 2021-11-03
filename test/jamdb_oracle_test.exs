a23ac6dbf95a7252d5b769381402352e33bc0bc9
Mykhailo Vstavskyi <vstavskyi@users.noreply.github.com>
2021-11-03 15:24:21 +0200

    Update jamdb_oracle_test.exs

--- test/jamdb_oracle_test.exs
+++ test/jamdb_oracle_test.exs
@@ -511,6 +511,9 @@ defmodule Jamdb.OracleTest do
   end
 
   test "tagged type" do
+    query = Schema |> select([], type(^"601d74e4-a8d3-4b6e-8365-eddb4c893327", :binary_id)) |> plan()
+    assert all(query) == ~s{SELECT HEXTORAW(:1) FROM schema s0}
+
     query = Schema |> select([], type(^"601d74e4-a8d3-4b6e-8365-eddb4c893327", Ecto.UUID)) |> plan()
     assert all(query) == ~s{SELECT HEXTORAW(:1) FROM schema s0}
   end
