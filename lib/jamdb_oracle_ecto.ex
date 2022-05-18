f99366308990c90b7c3050cffb0beaacf6568524
Mykhailo Vstavskyi <vstavskyi@users.noreply.github.com>
2022-05-18 12:59:54 +0300

    Update jamdb_oracle_ecto.ex

--- lib/jamdb_oracle_ecto.ex
+++ lib/jamdb_oracle_ecto.ex
@@ -130,15 +130,25 @@ defmodule Ecto.Adapters.Jamdb.Oracle.Connection do
     raise ArgumentError, msg
   end
 
+  @impl true
   defdelegate all(query), to: Jamdb.Oracle.Query
+  @impl true
   defdelegate update_all(query), to: Jamdb.Oracle.Query
+  @impl true
   defdelegate delete_all(query), to: Jamdb.Oracle.Query
+  @impl true
   defdelegate insert(prefix, table, header, rows, on_conflict, returning, placeholders), to: Jamdb.Oracle.Query
+  @impl true
   defdelegate update(prefix, table, fields, filters, returning), to: Jamdb.Oracle.Query
+  @impl true
   defdelegate delete(prefix, table, filters, returning), to: Jamdb.Oracle.Query
+  @impl true
   defdelegate table_exists_query(table), to: Jamdb.Oracle.Query
+  @impl true
   defdelegate execute_ddl(command), to: Jamdb.Oracle.Query
+  @impl true
   defdelegate ddl_logs(result), to: Jamdb.Oracle.Query
+  @impl true
   defdelegate to_constraints(err, opts), to: Jamdb.Oracle.Query
 
 end
