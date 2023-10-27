cfe2eb9d36919254e3752bc12e7e5eb5ecb3ad08
Mykhailo Vstavskyi <vstavskyi@users.noreply.github.com>
2023-10-27 11:33:07 +0300

    Update jamdb_oracle_conn.erl

--- src/jamdb_oracle_conn.erl
+++ src/jamdb_oracle_conn.erl
@@ -69,20 +69,17 @@ disconnect(#oraclient{socket=Socket, env=Env, passwd=Passwd}) ->
     freeval(Passwd),
     {ok, Env}.
 
--spec disconnect(state(), timeout()) -> ok.
-disconnect(#oraclient{conn_state=connected, socket=Socket, env=Env, passwd=Passwd} = State, _Tout) ->
-    _ = send_req(close, State),
+-spec disconnect(state(), timeout()) -> {ok, []}.
+disconnect(#oraclient{socket=Socket, passwd=Passwd} = State, _Tout) ->
+    send_req(close, State),
     sock_close(Socket),
     freeval(Passwd),
-    ok;
-disconnect(#oraclient{env=Env}, _Tout) ->
-    ok.
+    {ok, []}.
 
 -spec reconnect(state()) -> empty_result().
 reconnect(#oraclient{passwd=Passwd} = State) ->
     Passwd ! {get, self()},
     {Pass, NewPass} = receive Reply -> Reply end,
-    freeval(Passwd),
     {ok, EnvOpts} = disconnect(State),
     Pass2 = if NewPass =/= [] -> NewPass; true -> Pass end,
     connect([{password, Pass2}|EnvOpts]).
@@ -118,7 +115,7 @@ sql_query(#oraclient{conn_state=connected, timeouts={_Tout, ReadTout}} = State, 
         "PING" -> handle_req(tran, State, ?TTI_PING);
         "STOP" -> handle_req(stop, State, hd(Bind));
         "START" -> handle_req(spfp, State, []), handle_req(start, State, hd(Bind));
-        "CLOSE" -> disconnect(State, 1), {ok, [], State#oraclient{conn_state=disconnected}};
+        "CLOSE" -> send_req(close, State), handle_error(local, [], State);
         "CURRESET" -> send_req(reset, State), {ok, [], State};
         "TIMEOUT" -> {ok, [], State#oraclient{timeouts={hd(Bind), ReadTout}}};
         "FETCH" -> {ok, [], State#oraclient{fetch=hd(Bind)}};
@@ -177,9 +174,12 @@ handle_token(<<Token, Data/binary>>, State) ->
 
 handle_error(remote, Reason, State) ->
     {error, remote, Reason, State};
-handle_error(Type, Reason, State) ->
-    disconnect(State, 1),
-    {error, Type, Reason, State#oraclient{conn_state=disconnected}}.
+handle_error(socket, Reason, State) ->
+    disconnect(State),
+    {error, socket, Reason, State#oraclient{conn_state=disconnected}};
+handle_error(local, Reason, State) ->
+    disconnect(State),
+    {ok, Reason, State#oraclient{conn_state=disconnected}}.
 
 handle_bind(Query, Bind) ->
     Ks = string:tokens(Query," \t\r\n;,()="),
