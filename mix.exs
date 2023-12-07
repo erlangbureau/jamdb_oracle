34d227e13f581fb514ef94fde6b14a09558549b6
Mykhailo Vstavskyi <vstavskyi@users.noreply.github.com>
2023-12-07 08:22:16 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Jamdb.Oracle.Mixfile do
 
   def project do
     [app: :jamdb_oracle,
-     version: "0.5.7",
+     version: "0.5.8",
      elixir: "~> 1.10",
      description: description(),
      package: package(),
